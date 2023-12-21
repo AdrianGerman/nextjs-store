2e77907a49212b578c0b9016239aae4da9738a19
AdrianGerman <adrian.german1019@gmail.com>
2023-12-21 10:15:29 -0600

    adding statics

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "./Description.module.sass";
+
 export const Description = () => {
   return (
-    <section>
-      <h1>Descripción</h1>
+    <section className={styles.Description}>
+      <img src="/images/description.jpeg" alt="products marketplace" />
+      <div className={styles.Description__text}>
+        <h2>Trae el futuro hoy</h2>
+        <p>
+          Gikariu`s: ¡Su puerta de entrada a la tecnología del mañana! Sumérgete
+          en un mundo de dispositivos y equipos de última generación. Manténgase
+          a la vanguardia y redefina su estilo de vida digital con nosotros.
+        </p>
+      </div>
     </section>
   );
 };
