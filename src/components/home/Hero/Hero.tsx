f164d604d3d0d0bcb36c8318813e86654f172a61
AdrianGerman <adrian.german1019@gmail.com>
2023-12-21 08:19:15 -0600

    css module

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,10 @@
+import styles from "./Hero.module.css";
+
 export const Hero = () => {
   return (
-    <section>
-      <h1>Hero</h1>
+    <section className={styles.Hero}>
+      <h1>Gikariu`s</h1>
+      <h2>No hay pan que por café no venga!</h2>
     </section>
   );
 };
