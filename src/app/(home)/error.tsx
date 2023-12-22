99d5c7aa05732c5aa27ea48f54c767b9236ccdc7
AdrianGerman <adrian.german1019@gmail.com>
2023-12-22 15:39:35 -0600

    error design

--- src/app/(home)/error.tsx
+++ src/app/(home)/error.tsx
@@ -14,10 +14,54 @@ export default function Error({ error, reset }: ErrorProps) {
   }, []);
 
   return (
-    <div style={{ padding: "10rem" }}>
-      <h1>:c</h1>
-      <p>Ha ocurrido un error</p>
-      <button onClick={reset}>Intentar de nuevo</button>
+    <div
+      style={{
+        margin: "0",
+        textAlign: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        flexDirection: "column",
+        boxSizing: "border-box",
+      }}
+    >
+      <div
+        style={{
+          width: "50%",
+          border: "4px solid #d9534f",
+          borderRadius: "10px",
+        }}
+      >
+        <h1 style={{ fontSize: "3rem", color: "red" }}>
+          :(
+          <br />
+          <br />
+          Ha ocurrido un error
+          <br />
+        </h1>
+        <p>
+          ¡Posiblemente la página fallo o está realizando una demanda excesiva
+          en el sitio!
+        </p>
+
+        <button
+          style={{
+            padding: "0.5rem 1rem",
+            fontSize: "1rem",
+            color: "#fff",
+            backgroundColor: "#d9534f",
+            border: "1px solid #d9534f",
+            borderRadius: "4px",
+            cursor: "pointer",
+            marginTop: "1rem",
+          }}
+        >
+          {" "}
+          Recargar pagina{" "}
+        </button>
+        <br />
+        <br />
+      </div>
     </div>
   );
 }
