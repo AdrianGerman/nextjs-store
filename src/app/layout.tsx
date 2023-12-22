e2ac4599cb08f8993d9ff1c6de4dc5d0cced7fae
AdrianGerman <adrian.german1019@gmail.com>
2023-12-22 13:57:15 -0600

    loading component solution

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
+import { Hero } from "app/components/home/Hero";
+import { Description } from "app/components/home/Description";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import "app/sass/globals.sass";
@@ -23,6 +25,8 @@ export default function RootLayout({
     <html lang="es">
       <body className={ubuntu.className}>
         <Header />
+        <Hero />
+        <Description />
         {children}
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Hero } from "app/components/home/Hero";
-import { Description } from "app/components/home/Description";
 import { MainProducts } from "app/components/home/MainProducts";
 
 export default function Home() {
@@ -7,8 +5,6 @@ export default function Home() {
 
   return (
     <main>
-      <Hero />
-      <Description />
       <MainProducts />
     </main>
   );
