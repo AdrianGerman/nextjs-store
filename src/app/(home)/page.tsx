ce336f2ec5a61f5713cf58e9e3bf8c7167608156
AdrianGerman <adrian.german1019@gmail.com>
2023-12-26 10:14:21 -0600

    seo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Gikariu`s",
+  description: "Welcome to the gikariu`s, an ecommerce from other century",
+  keywords: ["ecommerce", "gikariu", "world", "technology"],
+};
 
 export default function Home() {
   console.log("Hola mundo desde la pagina de inicio");
