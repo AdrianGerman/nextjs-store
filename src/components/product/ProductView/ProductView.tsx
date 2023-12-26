deee151b6525c1d1ed5ddf60888505817e9ce9af
AdrianGerman <adrian.german1019@gmail.com>
2023-12-26 09:38:53 -0600

    redirects

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,11 +1,17 @@
+"use client";
 import Image from "next/image";
 import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
+import { useRouter } from "next/navigation";
 import styles from "./ProductView.module.sass";
 interface ProductViewProps {
   product: ProductType;
 }
 
 export const ProductView = ({ product }: ProductViewProps) => {
+  const router = useRouter();
+  if (!product) {
+    router.push("/");
+  }
   return (
     <main className={styles.ProductView}>
       <section className={styles.ProductView__images}>
