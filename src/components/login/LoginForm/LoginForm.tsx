313a053f72dc3fd96eca9bdcd63d52ae4ec8e32f
AdrianGerman <adrian.german1019@gmail.com>
2023-12-27 09:16:40 -0600

    login flow

--- src/components/login/LoginForm/LoginForm.tsx
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -1,11 +1,17 @@
 "use client";
+import { handleLogin } from "app/actions";
 import styles from "./LoginForm.module.sass";
 
 export const LoginForm = () => {
+  const handleSubmit = async (event: any) => {
+    const formData = new FormData(event.target);
+    event.preventDefault();
+    await handleLogin(formData);
+  };
   return (
     <div className={styles.NewAccountForm}>
       <h1 className={styles.NewAccountForm__title}>Login</h1>
-      <form className={styles.NewAccountForm__form}>
+      <form onSubmit={handleSubmit} className={styles.NewAccountForm__form}>
         <input
           type="text"
           name="email"
